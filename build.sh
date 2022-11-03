3bd39820cbf7049ae547c3e4ccdb0e915ecb60ca
derek.chen <derek.chen@iyes.dev>
2022-11-03 14:32:27 +0800

    feat: change build arch

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # This script is used to build the project.
 # It is assumed that the project is already configured.
 # Run Docker build
-docker build --build-arg GOOS=linux --build-arg GOARCH=arm64 --progress=plain -f drone-nolimit.Dockerfile -t test/drone-nolimit . 
+docker build --build-arg GOOS=linux --build-arg GOARCH=amd64 --progress=plain -f Dockerfile -t skymirror/drone:nolimit . 
